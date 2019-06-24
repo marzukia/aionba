92ed3d6faa25d50aab68b1cb06a9ebaf951c98a5
Andryo Marzuki <stabbish@gmail.com>
2019-06-25 07:39:27 +1200

    proxy function wip

--- aionba/core.py
+++ aionba/core.py
@@ -1,14 +1,13 @@
-import asyncio
 import aiohttp
 import aiosqlite
 
 import json
 from datetime import datetime
 
-from settings import SQLITE_PATH
+from .settings import SQLITE_PATH
 
 
-async def check_existing_query(db, url):cc
+async def check_existing_query(db, url):
     """ Checks local SQLite3 DB to see if requested URL is stored.
         If a table isn't found, one is created.
         TODO: Add a timer setting when re-cache needs to occur.
@@ -17,7 +16,7 @@ async def check_existing_query(db, url):cc
     try:
         cursor = await db.execute(sql)
     except aiosqlite.OperationalError:
-        await db.execute('CREATE TABLE query_cache(query VARCHAR, date DATETIME, response VARCHAR);')
+        await db.execute("CREATE TABLE query_cache(query VARCHAR, date DATETIME, response VARCHAR);")
         return None
     return await cursor.fetchone()
 
@@ -35,6 +34,15 @@ async def store_response(db, url, response):
     await db.commit()
 
 
+async def fetch_proxy():
+    """ Fetch new proxy from:
+        https://api.getproxylist.com/proxy
+    """
+    async with aiohttp.ClientSession() as session:
+        proxy = await session.get('https://api.getproxylist.com/proxy')
+        return await proxy.json()
+
+
 async def fetch_url(url):
     """ Check if URL is cached first via check_existing_query(),
         If none is found, fetch then store response.
@@ -44,9 +52,18 @@ async def fetch_url(url):
         query = await check_existing_query(db, url)
         if not query:
             async with aiohttp.ClientSession() as session:
-                response = await session.get(url)
+                proxy = await fetch_proxy()
+                response = await session.get(url, proxy=f"http://{proxy['ip']}:{proxy['port']}")
                 response = await response.json()
                 await store_response(db, url, json.dumps(response))
                 return response
         else:
             return json.loads(query[-1])
+
+
+def construct_url(endpoint):
+    """ Construct URL based on endpoint name.
+        https://github.com/seemethere/nba_py/wiki/stats.nba.com-Endpoint-Documentation
+        Documentation states https://stat.nba.com/stats/<endpoint>/?<params>
+    """
+    return f"https://stats.nba.com/stats/{endpoint}"
